6a3fd9351b7fcca581e379ca2f91c29da5a216a2
Your Name <you@example.com>
2021-01-09 20:54:57 +0100

    set up test sidebar menu with link to catalog page

--- src/app/main/_components/sidebar/sidebar.component.ts
+++ src/app/main/_components/sidebar/sidebar.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+const testMenu = [
+  {
+    name: 'Electronic',
+    children: [
+      {
+        name: 'Electronic_1',
+      }
+    ]
+  },
+];
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -7,6 +18,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  menu = testMenu;
   constructor() { }
 
   ngOnInit(): void {
